16eea3be91f7caac646a66c86e5012f8b6ec1996
varun <varunkarthik182004@gmail.com>
2026-03-06 19:44:36 +0530

    added scanner first implementation

--- src/Primitives.java
+++ src/Primitives.java
@@ -1,3 +1,11 @@
 public class Primitives {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        int a = 10;
+        char b = 'a';
+        float c = 10.5f;
+        double d = 10.5456830989345;
+        long e = 32498728357874892849732894L;
+        boolean x = false;
+
+    }
 }
